b7eff21b2341300e597f8af8522d3b635020bdab
Cristiana David <cristiana.radu92@yahoo.com>
2025-07-12 15:00:59 +0100

    Create home app

--- dvdine_burgers/urls.py
+++ dvdine_burgers/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from booking.views import index
+from home.views import about
 
 urlpatterns = [
-    path('booking/', index, name='booking'),
     path('admin/', admin.site.urls),
+    path('booking/', index, name='booking'),
+    path('home/', about, name='home'),
 ]

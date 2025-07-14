f0f5511ecdaabd07e93ddd4968d90514fd182f0b
Cristiana David <cristiana.radu92@yahoo.com>
2025-07-14 13:49:13 +0100

    BUG: Static files seem to respond with DEBUG=Tr ue not with False

--- dvdine_burgers/settings.py
+++ dvdine_burgers/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-_tt54xe-bie%tr81jurb6(*yx&eixt-!b&&q)ne3fu%7^l3oq0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com', '127.0.0.1',
@@ -138,7 +138,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
